a4bdd07c9bd7ab4b3c3e446ac44db4a0bb2a454d
vivek <vg6633004@gmail.com>
2020-01-19 04:16:41 -0500

    Add project files.

---
+++ CRUDWithEF/Models/PartialStudent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUDWithEF.Models
+{
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student
+    {
+    }
+
+    internal class StudentMetaData
+    {
+        [Required(ErrorMessage ="Id is the primary key. It's required.")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Name should not be blank.")]
+        public string Name { get; set; }
+
+        [Range(0.0, 5.0, ErrorMessage ="Grade should be within 0.0 to 5.0")]
+        [Required]
+        public Nullable<double> Grade { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name ="Faculty")]
+        public Nullable<int> FId { get; set; }
+    }
+}
